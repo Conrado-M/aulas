03d0d3b4df5a9d002ff33772d34fb2cba09a8cc4
Conrado Mastrocessario <conrado.m@gmail.com>
2023-08-15 23:06:39 -0300

    Update 15/08/2023

---
+++ controle/callBack2.js
@@ -0,0 +1,24 @@
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+// Sem callback
+const notasBaixas1 = []
+for (let i in notas) {
+    if (notas[i] < 7) {
+        notasBaixas1.push(notas[i])
+    }
+}
+
+console.log(notasBaixas1)
+
+// Com callback
+const notasBaixas2 = notas.filter(function (notas) {
+    return notas < 7
+})
+
+console.log(notasBaixas2)
+
+// Utilizando arrow
+const notaMenosQue7 = notas => notas < 7// funçao que pode ser utilizada em outro locais
+const notasBaixas3 = notas.filter(notaMenosQue7)
+
+console.log(notasBaixas3)
