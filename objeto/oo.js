edc8dc558de24e191b8b95030fefa4259f100c9f
Conrado Mastrocessario <conrado.m@gmail.com>
2023-08-18 19:47:10 -0300

    Update 18/08/2023-1

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NAO EXECUTAVEL!!!!
+
+// procedural
+processamento(valor1,valor2,valor3)
+
+// OO
+objejo ={
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objejo.processamento()  // Fóco passou a ser Objeto
+
+// Principior importantes:
+// 1° abstração
+// 2° encapsulamento
+// 3° herança (prototype
+// 4° Polimorfismo
