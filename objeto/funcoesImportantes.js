55d44dc0de0948fb1ac96d0805d47a0a49686a1b
Conrado Mastrocessario <conrado.m@gmail.com>
2023-08-23 21:05:52 -0300

    Update 23/08/2023-2

--- objeto/funcoesImportantes.js
+++ objeto/funcoesImportantes.js
@@ -8,7 +8,34 @@ console.log(Object.keys(pessoa))
 console.log(Object.values(pessoa))
 console.log(Object.entries(pessoa))
 
-Object.entries(pessoa).forEach(e => {
-    console.log(`${e[0]}: ${e[1]}`);
+
+// Utilizando 'e' para informas os elementos q serão utilizados
+
+// Object.entries(pessoa).forEach(e => {
+//     console.log(`${e[0]}: ${e[1]}`);
+// })
+
+// utilizando Destructuring para mais claros os elementos q serão utilizados
+Object.entries(pessoa).forEach(([chave, valor]) => { 
+    console.log(`${chave}: ${valor}`);
 })
 
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true, // se false não será exibido para métodos de iteração do objeto
+    writable: false, // se true essa prpriedade podera ser alterada (ex. linha 29)
+    value: '01/01/2019' // valor da variavel criada
+})
+
+pessoa.dataNascimento = '01/01/2017'
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
+
+// Object.assing
+const dest = {a: 1}
+const o1 = {b: 2}
+const o2 = {c: 3, a: 4} // 'a' sera subscrito
+const obj = Object.assign(dest,o1,o2)
+
+Object.freeze(obj)
+obj.c=1234 // 'c' não sera subscrito por conto do freeze
+console.log(obj)
