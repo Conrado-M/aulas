a408f21c524f7afe8058a252d82c55a5fa484696
Conrado Mastrocessario <conrado.m@gmail.com>
2023-10-02 12:12:27 -0300

    Update 02/10/2023-1

--- array/reduce3.js
+++ array/reduce3.js
@@ -1,7 +1,7 @@
 Array.prototype.reduce2 = function (callback, valorInicial) {
     const indiceInicial = valorInicial ? 0 : 1
     let acumulador = valorInicial || this[0]
-    for (let i = indiceInicial; i < this.length; ++i) {
+    for (let i = indiceInicial; i < this.length; i++) {
         acumulador = callback(acumulador, this[i], i, this)//função que vai ser executada para cada elemento
     }
     return acumulador
